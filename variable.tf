95915e04657bb339ceca7639883a6be713a75871
Tony Vattathil <avattathil@gmail.com>
2021-06-18 01:36:59 -0700

    Add interface for Codebuild resource

---
+++ variable.tf
@@ -0,0 +1,45 @@
+variable "region" {
+  description = "Codebuild region"
+  default     = "us-east-1"
+}
+
+variable "artifacts_type" {
+  description = "Type of artifacts (supports: NO_ARTIFACTS or CODEPIPELINE)"
+  default     = "NO_ARTIFACTS"
+}
+
+variable "project_name" {
+  description = "Project Name"
+}
+
+variable "git_repo" {
+  description = "Github repo "
+}
+
+variable "git_clone_depth" {
+  description = "Repo clone depth"
+  default     = "1"
+}
+
+variable "build_image" {
+  description = "Build container image"
+  default     = "aws/codebuild/amazonlinux2-x86_64-standard:3.0"
+}
+
+variable "compute_type" {
+  description = "Compute type"
+  default     = "BUILD_GENERAL1_MEDIUM"
+}
+
+variable "codebuild_logs_bucket" {
+  description = "Codebuild logs bucket"
+}
+
+variable "build_spec" {
+  description = "Build spec filename"
+}
+
+variable "tags" {
+  description = "Codebuild Tags"
+  default     = {}
+}
