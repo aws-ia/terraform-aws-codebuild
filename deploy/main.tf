4e75df595a0f5f74b57597804ce1fd943a61d4a5
Tony Vattathil <avattathil@gmail.com>
2021-06-22 16:36:41 -0700

    Add codebuild deployment

--- deploy/main.tf
+++ deploy/main.tf
@@ -11,3 +11,16 @@ terraform {
   backend "remote" {}
 }
 
+module "codebuild" {
+  source = "../"
+
+  build_image     = "aws/codebuild/amazonlinux2-x86_64-standard:3.0"
+  build_spec      = "./templates/build_spec/terraform_env.yml"
+  compute_type    = "BUILD_GENERAL1_MEDIUM"
+  environment     = "develop"
+  git_clone_depth = "1"
+  git_repo        = "https://github.com/aws-ia/terraform-modules-examples"
+  project_name    = "examples-codebuild-tf-deployment"
+  region          = "us-east-1"
+}
+
