397c123a6e24b42d600ceacfe4b7b966663bd8b2
Tony Vattathil <avattathil@gmail.com>
2021-06-22 17:08:43 -0700

    Paramertize

--- deploy/main.tf
+++ deploy/main.tf
@@ -16,10 +16,9 @@ module "codebuild" {
   build_image     = "aws/codebuild/amazonlinux2-x86_64-standard:3.0"
   build_spec      = "buildspecs/terraform_env.yml"
   compute_type    = "BUILD_GENERAL1_MEDIUM"
-  environment     = "develop"
+  environment     = var.environment
   git_clone_depth = "1"
-  git_repo        = "https://github.com/aws-ia/terraform-modules-examples"
-  project_name    = "examples-codebuild-tf-deployment"
-  region          = "us-east-1"
+  git_repo        = var.git_repo
+  project_name    = var.project_name
 }
 
