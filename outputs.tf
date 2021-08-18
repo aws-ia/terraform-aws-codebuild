c89d3935239af2dad9cfbd7cea57bb44766a2a36
Tony Vattathil <avattathil@gmail.com>
2021-08-18 14:17:49 -0700

    Add Badge Url to outputs

--- outputs.tf
+++ outputs.tf
@@ -8,3 +8,8 @@ output "codebuild_id" {
   value       = aws_codebuild_project.codebuild_project.id
 }
 
+output "codebuild_badge_url" {
+  description = "CodeBuild Badge Url"
+  value       = aws_codebuild_project.codebuild_project.badge_url
+}
+
