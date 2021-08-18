703fce2fe75c10f67ecdb4a2aa95f37660782e16
Tony Vattathil <avattathil@gmail.com>
2021-08-18 16:10:06 -0700

    Add outputs to deploy

---
+++ deploy/outputs.tf
@@ -0,0 +1,25 @@
+output "codebuild_arn" {
+  description = "CodeBuild Arn"
+  value       = module.codebuild.codebuild_arn
+}
+
+output "codebuild_id" {
+  description = "CodeBuild Id"
+  value       = module.codebuild.codebuild_id
+}
+
+output "codebuild_badge_url" {
+  description = "CodeBuild Badge Url"
+  value       = module.codebuild.codebuild_badge_url
+}
+
+output "codebuild_tags_all" {
+  description = "CodeBuild Badge Url"
+  value       = module.codebuild.codebuild_tags_all
+}
+
+output "codebuild_role_arn" {
+  description = "CodeBuild Role Arn"
+  value       = module.codebuild.codebuild_role_arn
+}
+
